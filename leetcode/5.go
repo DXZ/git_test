104f811884aab8d96bd5eba70b4ed51f87c0f6c4
dingxz <842375454@qq.com>
2018-07-03 21:05:53 +0800

    update

--- leetcode/5.go
+++ leetcode/5.go
@@ -11,12 +11,51 @@ func longestPalindrome(s string) string {
 }
 
 func longestPalindrome3(s string) (result string) {
-	l1 := len(s)
+	// l1 := len(s)
 	s1 := reverseString(s)
-	fmt.Println(l1, s1)
-	for i := 0; i < l1; i++ {
-		fmt.Println("11", i, s[i])
+	// fmt.Println(l1, s1)
+	runes1 := []rune(s1)
+	runes := []rune(s + s)
+	// result = result
+	fmt.Println(s, s1)
+	var result_tmp []rune
+	var result_runes []rune
+	for i := 0; i < len(runes); i++ {
+		k := i
+
+		for j := 0; j < len(runes1) && k < len(runes); j++ {
+			if len(runes1) == k {
+				fmt.Println("aaaa", k, j, string(result_tmp))
+				if len(result_tmp) > len(result_runes) && (k+j-len(result_tmp))%len(runes1) == 0 {
+					result_runes = result_tmp
+				}
+				result_tmp = []rune{}
+			}
+			if runes[k] == runes1[j] {
+				fmt.Println("111", string(runes[k]), k, j)
+				result_tmp = append(result_tmp, runes[k])
+				k++
+				continue
+			}
+			fmt.Println("bbbb", k, j, string(result_tmp), (k+j)%len(runes1), len(runes1))
+			if len(result_tmp) > len(result_runes) && (k+j-len(result_tmp))%len(runes1) == 0 {
+				result_runes = result_tmp
+			}
+			result_tmp = []rune{}
+			k++
+
+		}
+
+		if len(result_tmp) > len(result_runes) && (len(runes1)+k-len(result_tmp))%len(runes1) == 0 {
+			result_runes = result_tmp
+		}
+		result_tmp = []rune{}
 	}
+	if len(result_tmp) > len(result_runes) {
+		result_runes = result_tmp
+		result_tmp = []rune{}
+	}
+	result = string(result_runes)
 	return
 }
 
@@ -27,6 +66,7 @@ func reverseString(s string) string {
 	}
 	return string(runes)
 }
+
 func main() {
-	fmt.Println("sss:", longestPalindrome3("abbac"))
+	fmt.Println("sss:", longestPalindrome3("abcdasdfghjkldcba"))
 }
